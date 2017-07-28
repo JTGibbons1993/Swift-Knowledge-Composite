49c753674c8b79da4ee81fc7af9dab84a375757f
JTGibbons1993 <jtgibbons1993@gmail.com>
2017-07-28 13:12:29 -0400

    Initial Commit

---
+++ Swift-Knowledge-Composite.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit //type of SDK
+
+var str = "Do you want to play a game? Hehe.."
+
+var StringLesson: String = "Let's see, what do I know about strings? I know I can concatenate them like this: \(str) <-- This way, I can put in a variable in a string and now the value of that variable is a string, while still maintaining it's integrity as long as it's something straightforward and readable like a number as opposed to the location of a variable. Umm what else? I can also make numbers strings by writing the word String([insert variable here]) and putting a variable inside parenthesis. Also you can combine strings together like numbers like so: \(str+str) And that would combine the same string next to each other"
+
+var numLesson = "There are integer values, float values, and doubles, all of which can be converted into strings. There is a maximum integer value that an Int can have, though it is very large, so it's unnecessary to have it memorized. I don't know why they didn't choose a simple number. It would make things more easier like 1,000,000,000 or something like that. "
+
+var funcLesson = "Functions are the behavior of an object. It's what an object does just like how a cat has a function called meowing. It can meow and since the function is distinguisable from other object's functions, it's easy to tell what an object is from one simple function. There are other functions that are already stored in a Library of code called an SDK(Software Development Kit). There are also things called Delegate Methods/Functions that already exist within a class that Xcode provides. For example the methods needed to fill and allow a collectionView to work are found in an existing class in Xcode. That class must be inherited by the CollectionViewController to override those methods"
+
+var methodOverloading = ""
+
+var methodOverriding = ""
